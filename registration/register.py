79f9e0c770839c5af00f2024e3b6a9d4d8c001a9
Auden Woolfson <auden.woolfson@gmail.com>
2023-10-26 20:39:07 -0400

    tiers added

--- registration/register.py
+++ registration/register.py
@@ -1,31 +1,6 @@
 """
 Auden Woolfson, 2023
-This file contains the code responsible for 1. parsing the student and section data from CSV files,
-2. scoring students according to their answers to the survey questions, and 3. running the Gale-Shapley style
-algorithm to match students to sections. 4. (in progress) saving the matching to a CSV file
 
-Students are scored based on a number of factors. The first factor is whether thet are a major, 
-intended major, minor, or "none of the above". Majors will have first priority with 1000 as their starting base
-score, and intended majors will follow closely behind at 990. Minors and and "none of the above" students will have 
-a similar relationship, with starting base scores of 10 and 0 respectively. The rationale behind this is that
-majors will always be prioritized over minors unless the minor is in dire need of courses.
-
-The second factor students scores are based on is their graduation semester and the number of remaining courses
-they need to recieve the major/minor (0 if they are not on the major or minor track). They are given an additional 100 points
-for each course they need to take in order to reach the "on track" number of courses (2 per semester). This is to ensure
-no student gets too far behind. That being said, in order to prevent seniors who absolutely need their remaining courses from
-being blocked by juniors or sophomores who just need to catch up to a more manageable number of courses, graduating
-seniors are given +1200 to their base score. This dictates the ultimate priority of the scoring function, which is seniors
-who need to finish. It also does allow for cases in which senior minors who need courses can be prioritized over 
-majors who need courses less urgently. This is intentional.
-
-Students are also given an additional 50 points if they are graduating in Spring 2024, 40 points if they are graduating
-in Fall 2025, 30 points if they are graduating in Spring 2025, 20 points if they are graduating in Fall 2025, and 10 points
-if they are graduating in Spring 2026. These small sums act as tie breakers between students with otherwise identical scores.
-
-Note that the courses needed variable is bounded between 0 and 4. This is so students who are ahead of schedule
-(negative) or on schedule (0) will be prioritized the same. Students will not be punished for being ahead of schedule.
-It also prevents students who are very far behind from being prioritized over students who are only slightly behind.
 """
 
 import sys
@@ -43,6 +18,9 @@ from student import Student
 
 sections = section_csv_to_dict("cs_courses_spring24.csv")
 
+total_seats = sum(map(lambda x: x.capacity, sections.values()))
+remaining_seats = total_seats
+
 response_df =  pd.read_csv("google_form_students.csv")
 print(response_df)
 students = {}
@@ -59,17 +37,21 @@ for index, row in response_df.iterrows():
     status = row[3]
     
     if status == "declared CS major":
-        base_score = 1000
+        base_score = 3000
         courses_left = 12 - taken_count
+        major = "major"
     elif status == "intended CS major":
-        base_score = 990
+        base_score = 2000
         courses_left = 12 - taken_count
+        major = "intended"
     elif status == "CS minor":
-        base_score = 10
+        base_score = 1000
         courses_left = 5 - taken_count
+        major = "minor"
     else: # none of the above
         base_score = 0
         courses_left = 0
+        major = "none"
     
     # scores mainly based on number of courses needed to be "on track" (2 courses left per semester)
     grad_semester = row[5]
@@ -114,7 +96,9 @@ for index, row in response_df.iterrows():
     # courses_needed = min(courses_needed, 4)
     # base_score += courses_needed_soft * 100
     
-    section_limit = min(courses_needed_soft, row[19], 4)
+    # tier 1: majors needs
+    section_limit = min(courses_needed_soft, row[19], 4) if major == "major" else 0
+    remaining_seats -= section_limit
     
     crns = {
         "212-1": 10324,
@@ -141,7 +125,13 @@ for index, row in response_df.iterrows():
                 ranking.append(crn)
         
     new_student = Student(
-        id=row[2], name=row[1], major="NA", base_score=base_score,
+        id=row[2], name=row[1], major=major, base_score=base_score,
+        **{
+            "courses_needed_soft": courses_needed_soft,
+            "courses_needed_hard": courses_needed_hard,
+            "courses_desired": row[19],
+            "grad_semester": grad_semester,
+            }
     )
 
     new_student.set_section_ranking(ranking)
@@ -150,8 +140,71 @@ for index, row in response_df.iterrows():
 
     students[new_student.id] = new_student
 
-# add extra allocation of seats here
+prev_remaining_seats = remaining_seats
+while remaining_seats > 0:
+    print("looping", remaining_seats)
+    # tier 2: majors who haven't gotten any courses
+    for student in students.values():
+        if student.major == "minor" and \
+        student.kwargs["grad_semester"] == "Spring 2024" and \
+        student.kwargs["courses_needed_hard"] == 1 and \
+        student.section_limit < 1 and \
+        remaining_seats > 0:
+            student.section_limit += 1
+            remaining_seats -= 1
 
+    # tier 3: minors who need one last course
+    for student in students.values():
+        if student.section_limit == 0 and student.major == "major" and student.section_limit < 4 and remaining_seats > 0:
+            student.section_limit += 1
+            remaining_seats -= 1
+
+    # tier 4: graduating majors who want an extra course
+    for student in students.values():
+        if student.kwargs["grad_semester"] == "Spring 2024" and \
+        student.kwargs["courses_desired"] > student.section_limit and \
+        student.major == "major" and \
+        student.section_limit < 4 and \
+        remaining_seats > 0:
+            student.section_limit += 1
+            remaining_seats -= 1
+            
+    # tier 5: intended majors needs
+    for student in students.values():
+        if student.major == "intended":
+            while min(student.kwargs["courses_needed_soft"], student.kwargs["courses_desired"]) > student.section_limit and \
+            student.section_limit < 4 and \
+            remaining_seats > 0:
+                student.section_limit += 1
+                remaining_seats -= 1
+            
+    # tier 6: non seniro majors who want an extra course
+    for student in students.values():
+        if student.kwargs["grad_semester"] != "Spring 2024" and \
+        student.kwargs["courses_desired"] > student.section_limit and \
+        student.major == "major" and \
+        student.section_limit < 4 and \
+        remaining_seats > 0:
+            student.section_limit += 1
+            remaining_seats -= 1
+            
+    # tier 7: all minors who want an extra course
+    for student in students.values():
+        if student.major == "minor" and \
+        student.kwargs["courses_desired"] > student.section_limit and \
+        student.section_limit < 4 and \
+        remaining_seats > 0:
+            student.section_limit += 1
+            remaining_seats -= 1
+            
+    if prev_remaining_seats == remaining_seats:
+        break
+    prev_remaining_seats = remaining_seats
+            
+print(sum(map(lambda x: x.section_limit, students.values())))
+for student in students.values():
+    print(student.section_limit)
+            
 gale_shapley_match(students, sections)
 for section in sections.values():
     print(section)
